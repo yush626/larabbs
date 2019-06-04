4cbc43f9d113fb8599ea5a2d20d4e728a16026e1
yusheng <270957323@qq.com>
2019-06-04 13:11:05 +0000

    1

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="float-left">
+            <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
+    </div>
+</footer>
