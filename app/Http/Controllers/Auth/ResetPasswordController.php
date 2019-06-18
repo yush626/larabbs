35a2f94d8d29342977983172d229b2478c30586a
yusheng <270957323@qq.com>
2019-06-18 08:18:49 +0000

    找回密码提示

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -34,6 +34,7 @@ class ResetPasswordController extends Controller
      */
     public function __construct()
     {
+        session()->flash('success', '密码更新成功，您已成功登录！');
         $this->middleware('guest');
     }
 }
