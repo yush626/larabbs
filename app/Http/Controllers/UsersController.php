6e770a3a9b0879a8a1b93f92fe1bbce4bb5a1b78
yusheng <270957323@qq.com>
2019-06-19 05:38:27 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,6 +10,10 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['show']]);
+    }
     //
     public function show(User $user){
         return view('users.show', compact('user'));
@@ -20,6 +24,7 @@ class UsersController extends Controller
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user){
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
